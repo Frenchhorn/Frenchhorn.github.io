0369ab40dcc090901029f0d39a1632772ebf8990
Frenchhorn <nyanpasspass@outlook.com>
2017-03-30 11:31:00 +0800

    add yunzhidao

---
+++ yunzhidao/json/index.js
@@ -0,0 +1,42 @@
+window.jsonFile = window.jsonFile || {};
+window.jsonFile.index = {
+    "hotword": [
+        {
+            "displayWord": "防霾必看",
+            "searchWord": "",
+            "topArticles": [],
+            "tags": [],
+            "redirectLink": ""
+        }, {
+            "displayWord": "",
+            "searchWord": "霾",
+            "topArticles": [],
+            "tags": [],
+            "redirectLink": ""
+        }, {
+            "displayWord": "防霾必看",
+            "searchWord": "霾",
+            "topArticles": [],
+            "tags": [],
+            "redirectLink": ""
+        }, {
+            "displayWord": "防霾必看",
+            "searchWord": "霾",
+            "topArticles": [],
+            "tags": ["健康生活"],
+            "redirectLink": ""
+        }, {
+            "displayWord": "防霾必看",
+            "searchWord": "霾",
+            "topArticles": ["https://ch.amwaynet.com.cn/content/china/accl/ch/home/atmosphere/2016/1220017.html"],
+            "tags": ["健康生活"],
+            "redirectLink": "https://www.baidu.com"
+        }
+    ],
+    "category": {
+        "news": [],
+        "video": [],
+        "image": [],
+        "training": [],
+    }
+};
