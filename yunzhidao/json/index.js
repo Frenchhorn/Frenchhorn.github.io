d827aa260f631088afa21133b4558a3dd7c0e35d
Frenchhorn <nyanpasspass@outlook.com>
2017-04-02 16:22:59 +0800

    Add login page and edit landing page

--- yunzhidao/json/index.js
+++ yunzhidao/json/index.js
@@ -1,5 +1,6 @@
 window.jsonFile = window.jsonFile || {};
 window.jsonFile.index = {
+    "background": "url('images/bg_index.jpg')",
     "hotword": [
         {
             "displayWord": "防霾必看",
@@ -34,9 +35,208 @@ window.jsonFile.index = {
         }
     ],
     "category": {
-        "news": [],
-        "video": [],
-        "image": [],
-        "training": [],
+        "news": [
+            {
+                "name": "纽崔莱",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-1"
+            },{
+                "name": "雅姿",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-2"
+            },{
+                "name": "个人护理",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-3"
+            },{
+                "name": "家居科技",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-4"
+            },{
+                "name": "海外购",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-19"
+            },{
+                "name": "美好生活",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-20"
+            },{
+                "name": "走近安利",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-21"
+            },{
+                "name": "媒体看安利",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-22"
+            },{
+                "name": "事业机会",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-23"
+            },{
+                "name": "反击谣言",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-24"
+            },{
+                "name": "企业文化",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-33"
+            },{
+                "name": "九宫格",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-34"
+            }
+        ],
+        "video": [
+            {
+                "name": "健康",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-26"
+            },{
+                "name": "美丽",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-27"
+            },{
+                "name": "生活",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-28"
+            },{
+                "name": "事业",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-29"
+            },{
+                "name": "Amway",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-30"
+            },{
+                "name": "企业文化",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-35"
+            }
+        ],
+        "image": [
+            {
+                "name": "纽崔莱",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_4.png",
+                "color": "dark-green"
+            },{
+                "name": "雅姿",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_7.png",
+                "color": "brown"
+            },{
+                "name": "家居科技",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_2.png",
+                "color": "blue"
+            },{
+                "name": "优生活",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_3.png",
+                "color": "green"
+            },{
+                "name": "个人护理",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_5.png",
+                "color": "light-blue"
+            },{
+                "name": "海外购",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_1.png",
+                "color": "red"
+            },{
+                "name": "安利产品单",
+                "redirectLink": "",
+                "tags": "",
+                "image": "images/thumbnail/btn_list_6.png",
+                "color": "dark-blue"
+            },
+        ],
+        "training": [
+            {
+                "name": "纽崔莱",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-1"
+            },{
+                "name": "雅姿",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-2"
+            },{
+                "name": "家居科技",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-4"
+            },{
+                "name": "家居护理",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-5"
+            },{
+                "name": "个人护理",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-3"
+            },{
+                "name": "数字化",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-36"
+            },{
+                "name": "业务技能",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-6"
+            },{
+                "name": "制度和守则",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-7"
+            },{
+                "name": "礼仪修养",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-8"
+            },{
+                "name": "走近安利",
+                "redirectLink": "",
+                "tags": "",
+                "image": "icon-11"
+            }
+        ],
     }
 };
+
+
+
+
+
+
+
+
+
