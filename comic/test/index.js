09c576a638fd69c34374c697f6fea73c06476fd4
Frenchhorn <nyanpasspass@outlook.com>
2017-05-23 15:38:16 +0800

    Edit source file form and add edit show function in app

--- comic/test/index.js
+++ comic/test/index.js
@@ -5,20 +5,24 @@ var index =
     "comic":"东方铃奈庵",
     "author":"春河もえ/zun",
     "contributor":["kosuzu"],
+    "episode":[],
 },{
     "number":2,
     "comic":"四叶妹妹",
     "author":"东清彦",
     "contributor":["kosuzu"],
+    "episode":[],
 },{
     "number":3,
     "comic":"迷你偶像",
     "author":"明音",
     "contributor":["kosuzu", "test"],
+    "episode":[],
 },{
     "number":4,
     "comic":"东方三月精",
     "author":"比良坂真琴/zun",
     "contributor":["kosuzu", "test"],
+    "episode":[],
 }
 ]
