763d997ca5640c45078de01cbfdfdb021a6d8a81
Frenchhorn <nyanpasspass@outlook.com>
2017-05-29 18:55:20 +0800

    Format json files

--- comic/py/db.py
+++ comic/py/db.py
@@ -2,7 +2,7 @@ import os.path
 import datetime
 import peewee
 
-DATABASE = '../comic.db'
+DATABASE = os.path.join('..', 'comic.db')
 
 db = peewee.SqliteDatabase(DATABASE)
 
@@ -16,34 +16,32 @@ class Comic(BaseModel):
     comicID = peewee.IntegerField(unique=True)
     name = peewee.CharField()
     author = peewee.CharField(default='unknown')
-    location = peewee.CharField(default='unknown')
-    finished = peewee.BooleanField(default=False)
 
-    @staticmethod
-    def searchByName(name):
-        return Comic.select().where(Comic.name.contains(name))
+    # @staticmethod
+    # def searchByName(name):
+    #     return Comic.select().where(Comic.name.contains(name))
 
-    @staticmethod
-    def searchByAuthor(author):
-        return Comic.select().where(Comic.author.contains(author))
+    # @staticmethod
+    # def searchByAuthor(author):
+    #     return Comic.select().where(Comic.author.contains(author))
 
-    @staticmethod
-    def searchByLocation(location):
-        return Comic.select().where(Comic.location.contains(location))
+    # @staticmethod
+    # def searchByLocation(location):
+    #     return Comic.select().where(Comic.location.contains(location))
 
-    @staticmethod
-    def searchByAll(word):
-        return Comic.select().where(Comic.name.contains(word) |
-                                    Comic.author.contains(word) |
-                                    Comic.location.contains(word))
+    # @staticmethod
+    # def searchByAll(word):
+    #     return Comic.select().where(Comic.name.contains(word) |
+    #                                 Comic.author.contains(word) |
+    #                                 Comic.location.contains(word))
 
 
 class Episode(BaseModel):
     comic = peewee.ForeignKeyField(Comic, related_name='comic_episode')
     vol = peewee.IntegerField(null=True)
     episode = peewee.FloatField(null=True)
-    contributor = peewee.CharField()
-    date = peewee.DateTimeField(default=datetime.datetime.now())
+    special = peewee.CharField(null=True)
+    # contributor = peewee.CharField()
 
 
 class Page(BaseModel):
@@ -52,23 +50,9 @@ class Page(BaseModel):
     url = peewee.CharField()
 
 
-class File(BaseModel):
-    name = peewee.CharField(unique=True)
-    date = peewee.DateTimeField()
-
-    @staticmethod
-    def getFileDate(name):
-        try:
-            fileDate = File.select().where(File.name == name).get()
-            fileDate = fileDate.date.timestamp()
-            return fileDate
-        except:
-            return False
-
-
 def initDatabase():
     db.connect()
-    db.create_tables([Comic, Episode, Page, File])
+    db.create_tables([Comic, Episode, Page])
     db.close()
 
 
@@ -77,7 +61,6 @@ def test():
     Comic.create(comicID=2, name='12')
     c = Comic.create(comicID=3, name='123')
     Episode.create(comic=c, contributor='test')
-    File.create(name='t', date=datetime.datetime.now())
 
 
 if __name__ == '__main__':
--- comic/py/merge.py
+++ comic/py/merge.py
@@ -1,19 +1,27 @@
-import os.path
-from db import db, DATABASE, File
+import os
+import json
+from db import db, DATABASE
 
 
-def validFileTime(fileList):
-    newFileList = []
-    for file in fileList:
-        fileDate = File.getFileDate(file)
-        if not fileDate or fileDate == os.path.getmtime('source/' + file):
-            continue
-        newFileList.append(file)
-    return newFileList
+# def validFileTime(fileList):
+#     newFileList = []
+#     for file in fileList:
+#         if not fileDate or fileDate == os.path.getmtime('source/' + file):
+#             continue
+#         newFileList.append(file)
+#     return newFileList
 
 
-def validFile(fileList):
-    return fileList
+def readFile(fileList):
+    fileDict = {}
+    for i in fileList:
+        with open(i, 'rb') as f:
+            file = os.path.basename(i)
+            comic = f.read()
+            comic = comic.decode()
+            comic = json.loads(comic)
+            fileDict[file] = comic
+    return fileDict
 
 
 def insertDataBase(fileList):
@@ -24,10 +32,10 @@ def main():
     assert os.path.isfile(DATABASE), '双击db.py初始化数据库'
     db.connect()
     try:
-        assert not ('source' in os.listdir()), '目录source不存在'
-        fileList = os.listdir('source')
-        fileList = validFileTime(fileList)
-        fileList = validFile(fileList)
+        assert not ('source' in os.listdir('..')), '目录source不存在'
+        fileList = os.listdir(os.path.join('..', 'source'))
+        # fileList = validFileTime(fileList)
+        fileDict = readFile(fileList)
         insertDataBase(fileList)
     finally:
         db.close()
