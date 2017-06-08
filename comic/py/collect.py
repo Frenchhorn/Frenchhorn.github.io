ec8d561191c694139121714f3e3ae93a58ca866e
Frenchhorn <nyanpasspass@outlook.com>
2017-06-08 19:05:08 +0800

    Add function to update database

--- comic/py/collect.py
+++ comic/py/collect.py
@@ -1,6 +1,6 @@
 import os
 import json
-from db import db, DATABASE
+from db import db, DATABASE, Comic
 
 
 # def validFileTime(fileList):
@@ -23,8 +23,46 @@ def readFile(fileList):
     return fileDict
 
 
-def insertDataBase(fileList):
+def updateComic(comic, fileName):
+    comicObj = Comic.returnComic(comic.get('编号'))
+    if not comicObj:
+        comicObj = Comic.createComic(comic.get('编号'), comic.get('名称'), comic.get('作者'))
+    contributor = fileName[:fileName.rindex('.')]
+    updateEpisode(comic, comicObj, contributor)
+
+
+def updateEpisode(comic, comicObj, contributor):
+    vols = comic.get('卷')
+    episodes = comic.get('话')
+    specials = comic.get('番外')
+    if not vols:
+        for vol, pages in vols.items():
+            episodeObj = Episode.returnEpisode(comicObj, vol=vol)
+            if not episodeObj:
+                episodeObj = Episode.createEpisode(comicObj, contributor, vol=vol)
+            updatePage(episodeObj, pages)
+    if not episodes:
+        for episode, pages in episodes.items():
+            episodeObj = Episode.returnEpisode(comicObj, episode=episode)
+            if not episodeObj:
+                episodeObj = Episode.createEpisode(comicObj, contributor, episode=episode)
+            updatePage(episodeObj, pages)
+    if not specials:
+        for special, pages in item.items():
+            episodeObj = Episode.returnEpisode(comicObj, special=special)
+            if not episodeObj:
+                episodeObj = Episode.createEpisode(comicObj, contributor, special=special)
+            updatePage(episodeObj, pages)
+
+
+def updatePage():
     pass
+
+
+def insertDataBase(fileDict):
+    for fileName, value in fileDict.items():
+        for comic in value:
+            updateComic(comic, fileName)
 
 
 def main():
@@ -35,7 +73,7 @@ def main():
         fileList = os.listdir(os.path.join('..', 'source'))
         # fileList = validFileTime(fileList)
         fileDict = readFile(fileList)
-        insertDataBase(fileList)
+        insertDataBase(fileDict)
     finally:
         db.close()
 
--- comic/py/db.py
+++ comic/py/db.py
@@ -17,6 +17,19 @@ class Comic(BaseModel):
     name = peewee.CharField()
     author = peewee.CharField(null=True)
 
+    @staticmethod
+    def returnComic(comicID):
+        comicID = int(comicID)
+        query = Comic.select().where(Comic.comicID == comicID)
+        if len(query) == 0:
+            return None
+        else:
+            return query.get()
+
+    @staticmethod
+    def createComic(comicID, name, author):
+        return Comic.create(comicID=comicID, name=name, author=author)
+
     # @staticmethod
     # def searchByName(name):
     #     return Comic.select().where(Comic.name.contains(name))
@@ -38,10 +51,37 @@ class Comic(BaseModel):
 
 class Episode(BaseModel):
     comic = peewee.ForeignKeyField(Comic, related_name='comic_episode')
-    vol = peewee.FloatField(null=True)
+    vol = peewee.IntegerField(null=True)
     episode = peewee.FloatField(null=True)
     special = peewee.CharField(null=True)
-    # contributor = peewee.CharField()
+    contributor = peewee.CharField()
+
+    @staticmethod
+    def returnEpisode(comicObj, vol=None, episode=None, special=None):
+        if vol:
+            vol = int(vol)
+            query = Episode.select().where(Episode.comic == comicObj & Episode.vol == vol)
+        elif episode:
+            episode = float(episode)
+            query = Episode.select().where(Episode.comic == comicObj & Episode.episode == episode)
+        elif special:
+            query = Episode.select().where(Episode.comic == comicObj & Episode.special == special)
+
+        if len(query) == 0:
+            return None
+        else:
+            return query.get()
+
+    @staticmethod
+    def createEpisode(comicObj, contributor, vol=None, episode=None, special=None):
+        if vol:
+            vol = int(vol)
+            return Episode.create(comic=comicObj, contributor=contributor, vol=vol)
+        elif episode:
+            episode = float(episode)
+            return Episode.create(comic=comicObj, contributor=contributor, episode=episode)
+        elif special:
+            return Episode.create(comic=comicObj, contributor=contributor, special=special)
 
 
 class Page(BaseModel):
@@ -60,9 +100,9 @@ def test():
     Comic.create(comicID=1, name='东方铃奈庵', author='春河もえ/zun')
     Comic.create(comicID=2, name='四叶妹妹')
     comic3 = Comic.create(comicID=3, name='迷你偶像', author='明音')
-    vol1 = Episode.create(comic=comic3, vol=1)
-    Episode.create(comic=comic3, episode=1.5)
-    Episode.create(comic=comic3, special='1.5')
+    vol1 = Episode.create(comic=comic3, vol=1, contributor='kosuzu')
+    Episode.create(comic=comic3, episode=1.5, contributor='kosuzu')
+    Episode.create(comic=comic3, special='1.5', contributor='kosuzu')
     Page.create(episode=vol1, page=1, url='迷你偶像 卷1 页1')
 
 
