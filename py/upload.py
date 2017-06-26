ed61f32c7c1861a0b43d49eba88479f54338f0ab
Frenchhorn <nyanpasspass@outlook.com>
2017-06-26 14:20:42 +0800

    Move file upload.py to Project image2url

--- py/upload.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def uploadImage(path):
-    with open(path, 'rb') as f:
-        req = requests.post('https://sm.ms/api/upload', params={'ssl':True, 'format':'json'}, files={'smfile':f})
-        res = req.json()
-    return res
